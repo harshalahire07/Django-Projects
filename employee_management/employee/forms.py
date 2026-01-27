8ff1ff8015a875de739d3c30713a2aabe4c11813
Harshal <harshddahire@gmail.com>
2026-01-27 19:23:25 +0530

    Refactor to single-page CRUD using AJAX modals and ModelForms

---
+++ employee_management/employee/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Employee
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'department', 'role']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.NumberInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'department': forms.Select(attrs={'class': 'form-select'}),
+            'role': forms.Select(attrs={'class': 'form-select'}),
+        }
--- employee_management/employee/views.py
+++ employee_management/employee/views.py
@@ -1,118 +1,61 @@
-from django.shortcuts import render , HttpResponse, redirect
-from .models import Employee,Role,Department
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from .models import Employee
+from .forms import EmployeeForm
 
-def home(request):
-    return render(request, 'index.html')
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'index.html', {'employees': employees})
 
-def add_employee(request):
+def save_employee_form(request, form, template_name):
+    data = dict()
     if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        address = request.POST.get('address')
-        department_name = request.POST.get('department')
-        role_name = request.POST.get('role')
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            employees = Employee.objects.all()
+            data['html_employee_list'] = render_to_string('includes/partial_employee_list.html', {
+                'employees': employees
+            })
+        else:
+            data['form_is_valid'] = False
+    context = {'form': form}
+    data['html_form'] = render_to_string(template_name, context, request=request)
+    return JsonResponse(data)
 
-        if not all([first_name, last_name, email, phone, address, department_name, role_name]):
-             return render(request, 'add_employee.html', {'message': 'Missing fields'})
-
-        try:
-            phone = int(phone)
-        except ValueError:
-            return render(request, 'add_employee.html', {'message': 'Invalid phone number'})
-
-        try:
-            department = Department.objects.get(name=department_name)
-            role = Role.objects.get(name=role_name)
-        except (Department.DoesNotExist, Role.DoesNotExist):
-            return render(request, 'add_employee.html', {'message': 'Invalid Department or Role selected'})
-
-        employee = Employee(
-            first_name=first_name, 
-            last_name=last_name, 
-            email=email, 
-            phone=phone, 
-            address=address, 
-            department=department, 
-            role=role
-        )
-        employee.save()
-        return render(request, 'add_employee.html', {'message': 'Employee added successfully'})
-    elif request.method == 'GET':
-        return render(request, 'add_employee.html')
+def employee_create(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
     else:
-        return render(request, 'add_employee.html', {'message': 'Invalid request method'})
+        form = EmployeeForm()
+    return save_employee_form(request, form, 'includes/partial_employee_form.html')
 
-def remove_employee(request, employee_id=0):
-    context = {}
-    if employee_id:
-        try:
-            employee = Employee.objects.get(id=employee_id)
-            employee.delete()
-            context['message'] = "Employee removed successfully"
-        except Employee.DoesNotExist:
-            context['message'] = "Employee not found"
+def employee_update(request, id):
+    employee = get_object_or_404(Employee, id=id)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+    else:
+        form = EmployeeForm(instance=employee)
+    return save_employee_form(request, form, 'includes/partial_employee_form.html')
 
-    employees = Employee.objects.all()
-    context['employees'] = employees
-    
-    return render(request, 'remove_employee.html', context)
+def employee_delete(request, id):
+    employee = get_object_or_404(Employee, id=id)
+    data = dict()
+    if request.method == 'POST':
+        employee.delete()
+        data['form_is_valid'] = True
+        employees = Employee.objects.all()
+        data['html_employee_list'] = render_to_string('includes/partial_employee_list.html', {
+            'employees': employees
+        })
+    else:
+        context = {'employee': employee}
+        data['html_form'] = render_to_string('includes/partial_employee_delete.html', context, request=request)
+    return JsonResponse(data)
 
-def update_employee(request, employee_id=0):
-    context = {}
-    employees = Employee.objects.all()
-    context['employees'] = employees
-
-    if employee_id:
-        try:
-            employee = Employee.objects.get(id=employee_id)
-            context['employee'] = employee
-        except Employee.DoesNotExist:
-             context['message'] = "Employee not found"
-             return render(request, 'update_employee.html', context)
-
-        if request.method == 'POST':
-            first_name = request.POST.get('first_name')
-            last_name = request.POST.get('last_name')
-            email = request.POST.get('email')
-            phone = request.POST.get('phone')
-            address = request.POST.get('address')
-            department_name = request.POST.get('department')
-            role_name = request.POST.get('role')
-
-            if not all([first_name, last_name, email, phone, address, department_name, role_name]):
-                context['message'] = "Missing fields"
-                return render(request, 'update_employee.html', context)
-            
-            try:
-                phone = int(phone)
-                department = Department.objects.get(name=department_name)
-                role = Role.objects.get(name=role_name)
-                
-                employee.first_name = first_name
-                employee.last_name = last_name
-                employee.email = email
-                employee.phone = phone
-                employee.address = address
-                employee.department = department
-                employee.role = role
-                employee.save()
-                
-                context['message'] = "Employee updated successfully"
-               
-                context['employee'] = employee
-            except ValueError:
-                 context['message'] = "Invalid phone number"
-            except (Department.DoesNotExist, Role.DoesNotExist):
-                 context['message'] = "Invalid Department or Role"
-
-    return render(request, 'update_employee.html', context)
-
-def view_all_employees(request):
-    employees = Employee.objects.all()
-    context={   
-        'employees':employees
-    }
-    return render(request, 'view_all_employees.html',context)
+def employee_detail(request, id):
+    employee = get_object_or_404(Employee, id=id)
+    context = {'employee': employee}
+    html_form = render_to_string('includes/partial_employee_details.html', context, request=request)
+    return JsonResponse({'html_form': html_form})
